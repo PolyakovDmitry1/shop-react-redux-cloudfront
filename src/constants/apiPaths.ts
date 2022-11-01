0fe28e3368f0049052be939453d77a7e11ccb516
PolyakovDmitry1 <55856653+PolyakovDmitry1@users.noreply.github.com>
2022-11-02 01:11:48 +0200

    adding csv file import (#4)
    Co-authored-by: Dmytro Poliakov <dmytro_poliakov1@epam.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -5,6 +5,7 @@ const API_PATHS = {
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 
+  IMPORT_SERVICE: "https://0561i75y1k.execute-api.us-east-1.amazonaws.com/dev",
   PRODUCTS: "https://z1x6qo7d68.execute-api.us-east-1.amazonaws.com/dev",
 };
 
